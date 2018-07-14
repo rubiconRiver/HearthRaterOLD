9d15a387162811b1505657c7f69cfd797e967915
rubiconRiver <garett.gelsinger@gmail.com>
2018-07-14 09:28:00 -0600

    enlarged cards a bit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
