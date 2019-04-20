48d9afe39fc229e0d4c3516e4fe6e1391c23761b
rubiconRiver <garett.gelsinger@gmail.com>
2019-04-20 17:09:53 -0600

    ran npm update audit --force and corrected errors

--- src/Axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
- //   baseURL: 'https://hearthrater.firebaseio.com/'
-});
-
-export default instance;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
