1c7c3a386c95ac10c48537c17ea9aa96434cb5fa
rubiconRiver <garett.gelsinger@gmail.com>
2019-04-26 07:07:27 -0600

    added login component to git

---
+++ src/components/Login/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userJson, setUserJson] = useState(undefined);
+
+  function onSubmit() {
+    fetch("http://localhost:9000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(json => setUserJson(json));
+
+    console.log(userJson);
+  }
+
+  const changeEmail = event => {
+    setEmail(event.target.value);
+    console.log("email:" + email);
+  };
+
+  const changePassword = event => {
+    setPassword(event.target.value);
+    console.log("password:" + password);
+  };
+
+  return (
+    <div>
+      <h1>LOGIN PAGE!!!</h1>
+      <form>
+        <h4>username:</h4>
+        <input type="text" onChange={changeEmail} />
+        <h4>password:</h4>
+        <input type="text" onChange={changePassword} />
+        <div>
+          <button type="submit" onSubmit={onSubmit}>
+            login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
